401061fb96c872e479d6191b36617790f00e5c62
akeb0shi <dylan.z.kao@gmail.com>
2025-03-31 21:22:33 -0400

    oops

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,6 @@ import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
 import TicketList from './components/Tickets/TicketList';
 import CreateTicket from './components/Tickets/CreateTicket';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // import Guest from './pages/Guest'
 
 function App() {
