7026f7177d74bd7e00925bec74f6d9572ee4028b
Watson Maung <watson.maung@gmail.com>
2025-04-10 18:48:11 -0400

    Homepage css

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Home.css';
 
 function Home() {
   return (
-    <div>
-      <h1>Yoru Apparel Support Center</h1>
-      <p>Welcome to Yoru Apparel's customer support network! Please log in or register to submit a support ticket.</p>
-      <div>
-        <Link to="/login">Login</Link> | <Link to="/register">Register</Link>
+    <div className="home-container">
+      <div className="home-content">
+        <h1 className="home-title">Yoru Apparel Support Center</h1>
+        <p className="home-description">
+          Welcome to Yoru Apparel's customer support network! Please log in or register to submit a support ticket.
+        </p>
+        <div className="home-actions">
+          <Link to="/login" className="home-button login-button">Login</Link>
+          <Link to="/register" className="home-button register-button">Register</Link>
+        </div>
       </div>
     </div>
   );
